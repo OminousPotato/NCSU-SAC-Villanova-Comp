7f8fdf974b2d8f87a9210cfda729d8273b00977e
OminousPotato <67799535+OminousPotato@users.noreply.github.com>
2021-10-04 16:15:02 -0400

    Added Files

---
+++ WHKY Dataset.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/Brian/OneDrive/Documents/Programming/Sports Analytics Club/Nova-NCSU Competition/NC-State-Nova-Data-Viz-Challenge-2021-main")
+
+library(tidyverse)
+
+data <- read.csv("Clean PWHPA Secret Dream Gap Tour - SportLogiq - PWHPA Secret Dream Gap Tour.csv")
